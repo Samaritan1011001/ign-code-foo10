9790becbf0b6f71e18618dcce9d777dc6308dc36
Manoj <manojnb95@gmail.com>
2020-04-11 01:34:03 -0500

    Initial server code nodejs express

---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var MongoClient = require('mongodb').MongoClient
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'aaaaaaaa' });
+});
+
+module.exports = router;
